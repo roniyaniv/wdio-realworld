fb1b9d0c2866c0c62af1a0fee894e9dd01b2cd68
Roni Yaniv <roniyaniv@gmail.com>
2023-03-21 14:38:25 +0200

    switch to wdio expect assertion

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -2,16 +2,14 @@ describe('Homepage', function () {
   it('should load properly', async function () { 
     await browser.url('');
     
-    if (await browser.getTitle() !== 'Conduit'){
-      throw new Error('Title of the page should be "Conduit"');
-    };
+    expect(browser).toHaveTitle('Conduit');
 
     await $('=Sign in').click();
 
-    // console.log(await browser.getUrl());
+    expect(browser).toHaveUrl('/login', {containing: true});
 
-    if (await browser.getUrl() !== 'https://demo.learnwebdriverio.com/login'){
-      throw new Error('URL of the login page should be correct');
-    };
+    await $('=conduit').click();
+
+    expect(browser).toHaveTitle('Conduit');
   });
 });
